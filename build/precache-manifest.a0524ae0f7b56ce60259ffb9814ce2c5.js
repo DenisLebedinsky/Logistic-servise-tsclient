c646921fed052570557a438b13ef0846939416ba
Denis <Den.lebedinsky@gmail.com>
2020-01-31 02:03:38 +0300

    test build

---
+++ build/precache-manifest.a0524ae0f7b56ce60259ffb9814ce2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf15e34c879be57c5516e13e29ded98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a58e986555e6c64e912",
+    "url": "/static/css/main.97f32eaa.chunk.css"
+  },
+  {
+    "revision": "443ffa9b777a782d75eb",
+    "url": "/static/js/2.9f9fd8d3.chunk.js"
+  },
+  {
+    "revision": "8abc69bc0efadeae8e8063e574015577",
+    "url": "/static/js/2.9f9fd8d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a58e986555e6c64e912",
+    "url": "/static/js/main.2bcd2a11.chunk.js"
+  },
+  {
+    "revision": "843bba6a22398740a4d3",
+    "url": "/static/js/runtime-main.9aef1b08.js"
+  }
+]);
